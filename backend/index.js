865dd879f041385a323dcebd1b68e331f30b055e
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-03-25 19:10:17 -0400

    Show Book Details in React

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,14 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
-app.use(
-    cors({
-        origin : 'http:/localhost:3000', 
-        methods : ['GET', 'POST', 'PUT', 'DELETE'], 
-        allowedHeaders : ['Content-Type']
-    })
-)
+app.use(cors());
+// app.use(
+//     cors({
+//         origin : 'http:/localhost:3000', 
+//         methods : ['GET', 'POST', 'PUT', 'DELETE'], 
+//         allowedHeaders : ['Content-Type']
+//     })
+// )
 
 app.get("/", (request, response) => {
   console.log(response);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,10 @@ const App = () => {
   return (
    <Routes>
       <Route path='/' element={<Home />} />  
-      <Route path='/book/create' element={<CreateBook />} /> 
-      <Route path='/book/details/:id' element={<ShowBook />} /> 
-      <Route path='/book/edit/:id' element={<EditBook /> } /> 
-      <Route path='/book/delete/:id' element={<DeleteBook />} /> 
+      <Route path='/books/create' element={<CreateBook />} /> 
+      <Route path='/books/details/:id' element={<ShowBook />} /> 
+      <Route path='/books/edit/:id' element={<EditBook /> } /> 
+      <Route path='/books/delete/:id' element={<DeleteBook />} /> 
    </Routes>
   )
 }
