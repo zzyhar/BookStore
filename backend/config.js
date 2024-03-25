e8eed41e22dd2b5e4f561fbd09c2286e0e865bc7
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-03-25 07:40:09 -0400

    Add MongoDB and mongoose to node js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555; 
+
+export const mongoDBURL = 'mongodb+srv://zigarzahar:KskbkZNSUqAWHjNG@mern-book-store.fwiuwfl.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Book-Store';
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
-import  express  from "express";
-import {PORT} from "./config.js" 
-
+import  express, { request }  from "express";
+import {PORT, mongoDBURL} from "./config.js" 
+import mongoose from "mongoose";
 const app = express(); 
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port ${PORT}`)
+
+app.get('/', (request, response) => {
+    console.log(response); 
+    return response.status(200).send("Welcome to MERN Stack Book Store")
 })
+
+
+mongoose.connect(mongoDBURL)
+        .then(() => {
+            console.log("App connected to Database")
+            app.listen(PORT, () => {
+                console.log(`App is listening to port ${PORT}`);
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
