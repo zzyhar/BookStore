08b50f8374a0666aa407bbccc09fe161081a12a4
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-03-25 14:13:23 -0400

    Save a new Book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,48 @@
-import  express, { request }  from "express";
+import  express, { request, response }  from "express";
 import {PORT, mongoDBURL} from "./config.js" 
 import mongoose from "mongoose";
+import {Book} from './models/bookModel.js'
+
 const app = express(); 
+
+// Middleware for parsing request body
+app.use(express.json())
 
 
 app.get('/', (request, response) => {
     console.log(response); 
     return response.status(200).send("Welcome to MERN Stack Book Store")
 })
+
+app.post('/books', async (request, response) => { 
+    try{
+        if ( 
+        !request.title ||
+        !request.author ||
+        !request.publishYear 
+        ){
+            return response.status(400).send({
+                message : "Send all required fields: title, author, publishYear"
+            })
+        }
+
+        const newBook = { 
+            title : request.body.title, 
+            author : request.body.author, 
+            publishYear : request.body.publishYear, 
+        }
+
+        const book = await Book.create(newBook);
+        return response.status(201).send(book)
+
+
+
+    }catch(error){
+        console.log(error);
+        return response.status(500).send({message : error.message})
+    }
+})
+
 
 
 mongoose.connect(mongoDBURL)
