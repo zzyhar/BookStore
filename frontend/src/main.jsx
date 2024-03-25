d4078cbbe9880d479208e135beb536ad5ef9dccc
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-03-25 16:46:12 -0400

    Show Books List in React

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,10 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+     <App />
+    </React.StrictMode>,
+  </BrowserRouter>
+  
 )
